de36a507e7a0aa15ec2c5f35dd7d0ef49851b64c
tomj <118008765+thomasjjj@users.noreply.github.com>
2025-04-16 16:14:33 +0100

    Initial commit

---
+++ OSINT_REGEX.py
@@ -0,0 +1,13 @@
+import time
+
+if __name__ == "__main__":
+    start_time = time.time()
+    
+    
+    # Enter main code
+    
+    
+    # --End Code and Print Time Taken--
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Time taken: {elapsed_time:.2f} seconds")
